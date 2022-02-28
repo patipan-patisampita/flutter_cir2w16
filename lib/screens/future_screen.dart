f80187ff85b9f51b5e3e56e87c3f5e1b8886cee1
patipan-patisampita <patipan.patisampita@gmail.com>
2022-02-28 10:42:26 +0700

    update

--- lib/screens/future_screen.dart
+++ lib/screens/future_screen.dart
@@ -10,15 +10,32 @@ class FutureScreen extends StatefulWidget {
 }
 
 class _FutureScreenState extends State<FutureScreen> {
+  Future<void> getData() async {
+    Future.delayed(const Duration(seconds: 3), () {
+      print("Volodymyr Zelenskyy Id - 1");
+    }).then(
+      (value) {
+        Future.delayed(const Duration(seconds: 2), () {
+          print("Hey Vlademir Putin Id - 2");
+        }).catchError((error){
+          print(error);
+        });
+      },
+    );
+
+    print("Random Line of code");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Future Network"),
-      ),
+      appBar:
+          AppBar(title: Text("Future Network"), backgroundColor: Colors.green),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            getData();
+          },
           child: Text("Click here"),
         ),
       ),
